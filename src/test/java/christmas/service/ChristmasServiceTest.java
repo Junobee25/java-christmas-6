89315e880a7b84b41025ebd8f45be9b3292ecb64
Junobee25 <wnsdhqo@naver.com>
2023-12-13 12:37:40 +0900

    test(service): 총 구매 금액 계산 테스트 작성

--- src/test/java/christmas/service/ChristmasServiceTest.java
+++ src/test/java/christmas/service/ChristmasServiceTest.java
@@ -36,4 +36,23 @@ class ChristmasServiceTest {
         assertEquals(result1, 6246);
     }
 
+    @Test
+    void totalPurchase() {
+        //Given
+        Map<String, Integer> menu;
+        menu = Map.ofEntries(
+                Map.entry("티본스테이크", 1),
+                Map.entry("바비큐립", 1),
+                Map.entry("초코케이크", 2),
+                Map.entry("제로콜라", 1));
+
+
+        //When
+        int result1 = christmasService.totalPurchase(menu);
+
+
+        //Then
+        assertEquals(result1, 142000);
+    }
+
 }
