65e8f28e7ad696f4f57bf23f28be0a37d3d68a0c
Junobee25 <wnsdhqo@naver.com>
2023-12-13 12:37:15 +0900

    feat(service): 총 구매 금액 계산 메서드 작성

--- src/main/java/christmas/service/ChristmasService.java
+++ src/main/java/christmas/service/ChristmasService.java
@@ -1,8 +1,6 @@
 package christmas.service;
 
-import christmas.config.DessertType;
-import christmas.config.DiscountType;
-import christmas.config.MainType;
+import christmas.config.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,6 +10,108 @@ public class ChristmasService {
 
     private static final int ZERO = 0;
     private static final int ONE_THOUSAND = 1000;
+
+    public int totalPurchase(Map<String, Integer> order) {
+        int mainPrice = calculateMainPrice(order);
+        int dessertPrice = calculateDessertPrice(order);
+        int appetizerPrice = calculateAppetizerPrice(order);
+        int beveragePrice = calculateBeveragePrice(order);
+
+        return mainPrice + dessertPrice + appetizerPrice + beveragePrice;
+    }
+
+    private int calculateMainPrice(Map<String, Integer> order) {
+        int mainTotalPrice = 0;
+        List<String> mainMenu = Arrays.stream(MainType.values())
+                .map(MainType::getMenuName)
+                .toList();
+
+        for (Map.Entry<String, Integer> menu : order.entrySet()) {
+            if (mainMenu.contains(menu.getKey())) {
+                mainTotalPrice += calMainPrice(menu.getKey(), menu.getValue());
+            }
+        }
+        return mainTotalPrice;
+    }
+
+    private int calMainPrice(String menuName, int menuCount) {
+        for (MainType main : MainType.values()) {
+            if (menuName.equals(main.getMenuName())) {
+                return main.getPrice() * menuCount;
+            }
+        }
+        return ZERO;
+    }
+
+    private int calculateDessertPrice(Map<String, Integer> order) {
+        int dessertTotalPrice = 0;
+        List<String> dessertMenu = Arrays.stream(DessertType.values())
+                .map(DessertType::getMenuName)
+                .toList();
+
+        for (Map.Entry<String, Integer> menu : order.entrySet()) {
+            if (dessertMenu.contains(menu.getKey())) {
+                dessertTotalPrice += calDessertPrice(menu.getKey(), menu.getValue());
+            }
+        }
+        return dessertTotalPrice;
+    }
+
+    private int calDessertPrice(String dessertMenu, int dessertCount) {
+        for (DessertType dessert : DessertType.values()) {
+            if (dessertMenu.equals(dessert.getMenuName())) {
+                return dessert.getPrice() * dessertCount;
+            }
+        }
+        return 0;
+    }
+
+    private int calculateAppetizerPrice(Map<String, Integer> order) {
+        int appetizerTotalPrice = 0;
+        List<String> appetizerMenu = Arrays.stream(AppetizerType.values())
+                .map(AppetizerType::getMenuName)
+                .toList();
+
+        for (Map.Entry<String, Integer> menu : order.entrySet()) {
+            if (appetizerMenu.contains(menu.getKey())) {
+                appetizerTotalPrice += calAppetizerPrice(menu.getKey(), menu.getValue());
+            }
+        }
+        return appetizerTotalPrice;
+    }
+
+    private int calAppetizerPrice(String appetizerMenu, int appetizerCount) {
+        for (AppetizerType appetizer : AppetizerType.values()) {
+            if (appetizerMenu.equals(appetizer.getMenuName())) {
+                return appetizer.getPrice() * appetizerCount;
+            }
+        }
+        return 0;
+    }
+
+
+    private int calculateBeveragePrice(Map<String, Integer> order) {
+        int beverageTotalPrice = 0;
+        List<String> beverageMenu = Arrays.stream(BeverageType.values())
+                .map(BeverageType::getMenuName)
+                .toList();
+
+        for (Map.Entry<String, Integer> menu : order.entrySet()) {
+            if (beverageMenu.contains(menu.getKey())) {
+                beverageTotalPrice += calBeveragePrice(menu.getKey(), menu.getValue());
+            }
+        }
+        return beverageTotalPrice;
+    }
+
+    private int calBeveragePrice(String beverageMenu, int beverageCount) {
+        for (BeverageType beverage : BeverageType.values()) {
+            if (beverageMenu.equals(beverage.getMenuName())) {
+                return beverage.getPrice() * beverageCount;
+            }
+        }
+        return 0;
+    }
 
     public int totalDiscount(int date, Map<String, Integer> order) {
         int christmasDiscount = calculateChristmasDiscount(date);
