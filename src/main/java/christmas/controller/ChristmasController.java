43eb7ff733b13b2382eddadedd7dfa5fa0cc2072
Junobee25 <wnsdhqo@naver.com>
2023-12-13 18:22:52 +0900

    feat(controller): 총 할인 금액 계산 controller 추가

---
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -0,0 +1,57 @@
+package christmas.controller;
+
+import christmas.domain.Date;
+import christmas.domain.Order;
+import christmas.service.ChristmasService;
+import christmas.util.Utils;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+import java.util.Map;
+
+public class ChristmasController {
+
+    private final ChristmasService christmasService;
+
+    public ChristmasController() {
+        christmasService = new ChristmasService();
+    }
+
+    public void promotionEvent() {
+        int date = inputDate();
+        Map<String, Integer> order = inputOrder();
+
+    }
+
+    private void showOrderResult(int date, Map<String ,Integer> order) {
+        OutputView.previewBenefit(date);
+        OutputView.orderMenu(order);
+        OutputView.previewOrderPriceBeforeDiscount(christmasService.totalPurchase(order));
+    }
+
+    public int inputDate() {
+        InputView.welcome();
+
+        while (true) {
+            try {
+                Date date = new Date(Utils.stringToInteger());
+                return date.getDate();
+            } catch (IllegalArgumentException e) {
+            }
+        }
+    }
+
+    public Map<String, Integer> inputOrder() {
+        InputView.inputOrderAndCount();
+
+        while (true) {
+            try {
+                Order order = new Order(Utils.splitStringViaDash());
+                return order.getOrder();
+            } catch (IllegalArgumentException e) {
+
+            }
+        }
+    }
+
+}
