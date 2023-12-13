21b464547713e93b664213ee7c66fd56e1a4713f
Junobee25 <wnsdhqo@naver.com>
2023-12-13 12:02:59 +0900

    feat(configuration): 총 할인 금액 계산에 필요한 Enum 타입 추가

---
+++ src/main/java/christmas/config/AppetizerType.java
@@ -0,0 +1,24 @@
+package christmas.config;
+
+public enum AppetizerType {
+    MUSHROOM_SOUP("양송이수프", 6000),
+    TAPAS("타파스", 5500),
+    CAESAR_SALAD("시저샐러드", 8000);
+
+    private final String appetizer;
+    private final int price;
+
+    AppetizerType(String appetizer, int price) {
+        this.appetizer = appetizer;
+        this.price = price;
+    }
+
+    public String getMenuName() {
+        return appetizer;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+}
---
+++ src/main/java/christmas/config/DessertType.java
@@ -0,0 +1,22 @@
+package christmas.config;
+
+public enum DessertType {
+    CHOCOLATE_CAKE("초코케이크", 15000),
+    ICE_CREAM("아이스크림", 5000);
+
+    private final String dessert;
+    private final int price;
+
+    DessertType(String dessert, int price) {
+        this.dessert = dessert;
+        this.price = price;
+    }
+
+    public String getMenuName() {
+        return dessert;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
