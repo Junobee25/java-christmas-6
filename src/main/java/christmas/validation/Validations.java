29142ca3a9188454e3744f751054a020e5c5db1c
Junobee25 <wnsdhqo@naver.com>
2023-12-13 22:36:36 +0900

    feat(validation): 메뉴 입력 추가 예외 케이스 작성

--- src/main/java/christmas/validation/Validations.java
+++ src/main/java/christmas/validation/Validations.java
@@ -1,10 +1,10 @@
 package christmas.validation;
 
 import christmas.config.Menu;
+import christmas.config.MenuType;
 import christmas.view.ErrorMessage;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class Validations {
@@ -62,5 +62,46 @@ public class Validations {
             ErrorMessage.inputOrderError();
             throw new IllegalArgumentException();
         }
+    }
+
+    public static void validateOnlyBeverage(Map<String, Integer> order) {
+        Set<MenuType> distinctMenuTypes = new HashSet<>();
+        MenuType[] menuTypes = MenuType.values();
+
+        for (MenuType menuType : menuTypes) {
+            distinctMenuTypes.addAll(addMenuType(menuType, order));
+        }
+
+        if (distinctMenuTypes.size() == 1 && distinctMenuTypes.contains(MenuType.BEVERAGE)) {
+            ErrorMessage.inputOrderError();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateMenuCountOver(Map<String, Integer> order) {
+        int totalCount = 0;
+
+        for(Map.Entry<String, Integer> menu : order.entrySet()) {
+            totalCount += menu.getValue();
+        }
+
+        if (totalCount > 20) {
+            ErrorMessage.inputOrderError();
+            throw new IllegalArgumentException();
+        }
+
+
+    }
+
+    private static Set<MenuType> addMenuType(MenuType menuType, Map<String, Integer> order) {
+        Set<MenuType> menuTypes = new HashSet<>();
+
+        for (Map.Entry<String, Integer> menu : order.entrySet()) {
+            if (menuType.getMenuList().contains(menu.getKey())) {
+              menuTypes.add(menuType);
+            }
+        }
+
+        return menuTypes;
     }
 }
