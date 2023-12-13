b0d18f328463962658b34687e7d8f02afef6bf96
Junobee25 <wnsdhqo@naver.com>
2023-12-13 18:21:42 +0900

    feat(domain): 주문, 할인 금액 계산에 필요한 domain 모델 추가

---
+++ src/main/java/christmas/domain/Date.java
@@ -0,0 +1,14 @@
+package christmas.domain;
+
+public class Date {
+
+    private final int date;
+
+    public Date(int date) {
+        this.date = date;
+    }
+
+    public int getDate() {
+        return date;
+    }
+}
---
+++ src/main/java/christmas/domain/Order.java
@@ -0,0 +1,16 @@
+package christmas.domain;
+
+import java.util.Map;
+
+public class Order {
+
+    private final Map<String, Integer> order;
+
+    public Order(Map<String, Integer> order) {
+        this.order = order;
+    }
+
+    public Map<String, Integer> getOrder() {
+        return order;
+    }
+}
