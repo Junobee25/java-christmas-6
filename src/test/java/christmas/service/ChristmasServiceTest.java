32acea5cf181504f4a522d413740eb90f9d3fad2
Junobee25 <wnsdhqo@naver.com>
2023-12-13 12:03:20 +0900

    test(service): 총 할인 금액 계산 테스트 코드 작성

---
+++ src/test/java/christmas/service/ChristmasServiceTest.java
@@ -0,0 +1,39 @@
+package christmas.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChristmasServiceTest {
+    private ChristmasService christmasService;
+
+    @BeforeEach
+    void setUp() {
+        christmasService = new ChristmasService();
+    }
+
+    @Test
+    void totalDiscount() {
+        //Given
+        Map<String, Integer> menu;
+        menu = Map.ofEntries(
+                Map.entry("티본스테이크", 1),
+                Map.entry("바비큐립", 1),
+                Map.entry("초코케이크", 2),
+                Map.entry("제로콜라", 1));
+
+        int date = 3;
+
+        //When
+        int result1 = christmasService.totalDiscount(3, menu);
+
+
+        //Then
+        assertEquals(result1, 6246);
+    }
+
+}
